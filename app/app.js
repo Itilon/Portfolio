98c5c540c7e9a809734e6f68facc832e37c69b6a
Itilon <ivanov.elect@gmail.com>
2017-12-23 20:20:12 +0200

    Initial Design Setup

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
+/* globals __dirname */
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const init = (data) => {
     const app = express();
@@ -7,6 +9,14 @@ const init = (data) => {
     app.set('view engine', 'pug');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/static', express.static(
+        path.join(__dirname, '../static'))
+    );
+
+    app.use('/libs', express.static(
+        path.join(__dirname, '../node_modules'))
+    );
 
     app.get('/', (req, res) => {
         return res.render('home');
